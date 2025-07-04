b5c4a43546233f92fb800ee3d8cc9b5a3973ca6b
hettfield git <olekpl90@gmail.com>
2025-07-04 12:03:52 +0200

    added form into single.php

--- single.php
+++ single.php
@@ -4,22 +4,29 @@ get_header();
 if ( have_posts() ) :
     while ( have_posts() ) : the_post(); ?>
     
-    <main class="single-post container">
-        <article>
-            <h1><?php the_title(); ?></h1>
-            <p class="post-meta">
-                <?php echo get_the_date(); ?> |
-                <?php the_category(', '); ?>
-            </p>
-            
-            <?php if ( has_post_thumbnail() ) {
-                the_post_thumbnail('large');
-            } ?>
+    <main>
+        <div class="container">
+            <div class="vertical-and-form">
+                <article>
+                    <h1><?php the_title(); ?></h1>
+                    <p class="post-meta">
+                        <?php echo get_the_date(); ?> |
+                        <?php the_category(', '); ?>
+                    </p>
+                    
+                    <?php if ( has_post_thumbnail() ) {
+                        the_post_thumbnail('large');
+                    } ?>
 
-            <div class="post-content">
-                <?php the_content(); ?>
+                    <div class="post-content">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+                <div class="form-block">
+                    <?php echo do_shortcode('[fluentform id="2"]'); ?>
+                </div>
             </div>
-        </article>
+        </div>
     </main>
 
     <?php endwhile;
